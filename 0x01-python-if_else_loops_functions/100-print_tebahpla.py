a9004c21fa06da72e3c4990f56e8aa29dec47897
SuperMegacy <pilusaemanuel@gmail.comc>
2024-09-10 10:20:44 +0200

    aBcD

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 print(
     ''.join(
-        chr(122 - i).upper() if i % 2 != 0 else chr(122 - i)
+        '{}'.format(chr(122 - i).upper() if i % 2 != 0 else chr(122 - i))
         for i in range(26)
      )
 )
